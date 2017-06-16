54693c045fbc6ea0ad6d45e6f1369f6601742c44
Thuan Nguyen <ninhthuan82us@gmail.com>
2017-06-16 16:48:41 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ rm -f check-mk-agent_1.2.8p20-1_all.deb
 wget http://ftp.lihas.de/debian/packages/check-mk-agent_1.2.8p20-1_all.deb
 sudo apt-get install xinetd -y
 sudo dpkg -i check-mk-agent_1.2.8p20-1_all.deb
-cp ./check_mk.conf /etc/xinetd.d/check_mk
+sudo rm -f /etc/xinetd.d/check_mk
+sudo cp ./check_mk.conf /etc/xinetd.d/check_mk
 sudo /etc/init.d/xinetd restart
 
 # Open firewall allow access from salt to agent
